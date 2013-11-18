54728a22363549b39091f5fd9ba3791ecc3b9c0a
Максим Понтюшенко <Maxim@air-maksim.dlink>
2013-11-18 17:11:09 +0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JORDMARKET::Application.config.secret_token = 'f3c6adda508f3fba2df238c95843707c626f0833cd615c7b2585790b7a1342eb84c74d3af409a6b863a0ca61e24f9b370e3726cef88ed9ded53ca85c432b56a9'
